dcc48a3141590946ee2a66f13911aea3960d98fa
Jarek Styba <jarek.styba@gmail.com>
2018-09-05 22:41:29 +0200

    request from backend works from service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,19 @@
 import {Component, EventEmitter, Output} from '@angular/core';
 
+import {BackEndConnectionService} from './dict/back-end-connection-service';
+
 import {HttpModule, Http, Response, Headers, RequestOptions, JSONPBackend} from '@angular/http';
 import {HttpClient, HttpClientModule, HttpHeaders, HttpParams} from '@angular/common/http';
-import {observable, Observable} from 'rxjs';
-import {post} from 'selenium-webdriver/http';
-import {forEach} from '@angular/router/src/utils/collection';
-import {stringify} from 'querystring';
-import {decoratorArgument} from 'codelyzer/util/astQuery';
-import {jsonpFactory} from '@angular/http/src/http_module';
-import {catchError} from 'rxjs/operators';
-import {tryCatch} from 'rxjs/internal-compatibility';
+import {DefinitionsPipePipe} from './dict/definitions-pipe.pipe';
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  providers: [BackEndConnectionService, DefinitionsPipePipe]
 
 })
 
@@ -22,19 +21,16 @@ import {tryCatch} from 'rxjs/internal-compatibility';
 
 export class AppComponent {
   @Output() userClick: EventEmitter<any> = new EventEmitter();
+  private jsonResponse: any = null;
 
-  public constructor(private http: HttpClient) {
+  public constructor(private http: HttpClient, private backendConnectionService: BackEndConnectionService
+  , private definitionsPipe: DefinitionsPipePipe) {
   }
     parser = new DOMParser();
   word = null;
-  definitions = null;
-  jsonString = null;
-  tmpWordTranslation = null;
+  definitions;
   tableOfWords: String[] = [];
   tmpWord: String = '';
-  httpOptions = {
-
-
   selectWord() {
     const s = window.getSelection();
     const range = s.getRangeAt(0);
@@ -53,7 +49,7 @@ export class AppComponent {
     for (let i = 0; i < this.tableOfWords.length; i++) {
       console.log(this.tableOfWords[i]);
       this.tmpWord = str;
-    }
+      }
   }
 
   removeWord(element, array) {
@@ -63,25 +59,14 @@ export class AppComponent {
       array.splice(index, 1);
     }
   }
+  // setDefinitions () {
+  //   this.definitions = this.backendConnectionService.getDefinitions;
+  // }
 
-
-  getResponse(word) {
-
-    const username = 'user';
-    const password = 'password1';
-    const auth = btoa(username + ':' + password);
-    const params = new HttpParams().set('word', word);
-    const headers = {'FOO': 'foo', 'Content-Type': 'application/json', 'Accept': 'application/json', 'Cache-Control': 'no-cache'};
-    this.http.get('http://localhost:8080/', {params, headers}).subscribe(json => {
-      console.log(json);
-      this.word = json.word;
-      this.definitions = json.definitions;
-      // this.tmpWordTranslation = JSON.parse(json);
-      console.log('this should come from word: ' + this.word);
-      for (let i = 0 ; i < this.definitions.length ; i++){
-        console.log(i + ': ' + this.definitions[i]);
-      }
-    });
+  getJsonResponse (word): void {
+    // console.log(this.backendConnectionService.getResponse(word));
+    this.definitions = this.backendConnectionService.getResponse(word);
+    // this.backendConnectionService.getResponse(word);
     }
 }
 
