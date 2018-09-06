13b3077693bff54a879b03cd5a7014ff03994dd6
Jarek Styba <jarek.styba@gmail.com>
2018-09-06 11:06:02 +0200

    small refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,27 @@
 import {Component, EventEmitter, Output} from '@angular/core';
-
 import {BackEndConnectionService} from './dict/back-end-connection-service';
-
-import {HttpModule, Http, Response, Headers, RequestOptions, JSONPBackend} from '@angular/http';
 import {HttpClient, HttpClientModule, HttpHeaders, HttpParams} from '@angular/common/http';
-import {DefinitionsPipePipe} from './dict/definitions-pipe.pipe';
-
-
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [BackEndConnectionService, DefinitionsPipePipe]
+  providers: [BackEndConnectionService]
 
 })
 
-
-
 export class AppComponent {
   @Output() userClick: EventEmitter<any> = new EventEmitter();
-  private jsonResponse: any = null;
 
-  public constructor(private http: HttpClient, private backendConnectionService: BackEndConnectionService
-  , private definitionsPipe: DefinitionsPipePipe) {
+  public constructor(private http: HttpClient, private backendConnectionService: BackEndConnectionService) {
   }
-    parser = new DOMParser();
+
+  parser = new DOMParser();
   word = null;
   definitions;
   tableOfWords: String[] = [];
   tmpWord: String = '';
+
   selectWord() {
     const s = window.getSelection();
     const range = s.getRangeAt(0);
@@ -49,7 +40,7 @@ export class AppComponent {
     for (let i = 0; i < this.tableOfWords.length; i++) {
       console.log(this.tableOfWords[i]);
       this.tmpWord = str;
-      }
+    }
   }
 
   removeWord(element, array) {
@@ -59,15 +50,10 @@ export class AppComponent {
       array.splice(index, 1);
     }
   }
-  // setDefinitions () {
-  //   this.definitions = this.backendConnectionService.getDefinitions;
-  // }
 
-  getJsonResponse (word): void {
-    // console.log(this.backendConnectionService.getResponse(word));
+  getJsonResponse(word): void {
     this.definitions = this.backendConnectionService.getResponse(word);
-    // this.backendConnectionService.getResponse(word);
-    }
+  }
 }
 
 // && range.endOffset < node.length - might need it later
