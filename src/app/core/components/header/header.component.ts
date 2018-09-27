32e17594b3f7e8e7ac1c5769983d828ba6ab93ac
gosiastyba <gosia.werecka@gmail.com>
2018-09-27 14:11:24 +0200

    Create sticky menu on scroll

---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,27 @@
+import {Component, ElementRef, HostListener, OnInit, ViewChild, AfterViewInit} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements AfterViewInit {
+  @ViewChild('StickyTopNav') menuElement: ElementRef;
+  topNav = false;
+  elementPosition: any;
+  constructor() { }
+  ngAfterViewInit() {
+    this.elementPosition = this.menuElement.nativeElement.offsetTop;
+  }
+  @HostListener('window:scroll', [ '$event'])
+    handleScroll() {
+    const windowScroll = window.pageYOffset;
+    console.log( 'test' + this.elementPosition);
+    if (windowScroll >= this.elementPosition) {
+      this.topNav = true;
+    } else {
+      this.topNav = false;
+    }
+  }
+}
+
